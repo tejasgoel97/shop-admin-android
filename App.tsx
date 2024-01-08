fb66f05ae799055f20d49a3f73da7f3f58da4f9c
Tejas <tejas.goel786@gmail.com>
2024-01-08 13:42:36 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hieelo</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
